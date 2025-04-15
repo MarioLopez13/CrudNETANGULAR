883c65f61012de38c015d2b4a3e643772fad999d
Mario <mario.lopez.mena@udla.edu.ec>
2025-04-15 11:07:27 -0500

    Commit con la clase usuario para login

---
+++ MiWebAPI/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace MiWebAPI.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string? NombreUsuario { get; set; }
+        public string? Contrasena { get; set; } // No se usará para recibir, solo para lógica interna
+    }
+
+    public class UsuarioLogin
+    {
+        public string? NombreUsuario { get; set; }
+        public string? Contrasena { get; set; }
+    }
+}
